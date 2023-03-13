318cd8fa1500f516636666a630ece71e97161619
michaelkot97 <michael.kot97@gmail.com>
2023-03-13 11:01:25 +0200

    Added fix

--- service-generator.sh
+++ service-generator.sh
@@ -1,9 +1,9 @@
 for ((i=1; i<=40; i++))
 do
-    cp -r list-app-3 list-app-2-$i
-    find list-app-2-$i/ -type f | xargs sed -i "s/3/$i/g"
-    echo "  list-app-2-$i:" >> appset-2/values.yaml
-    echo "    repoURL: https://github.com/michaelkotelnikov/argocd-bench-list-generator.git" >> appset-2/values.yaml
-    echo "    targetRevision: master" >> appset-2/values.yaml
-    echo "    path: list-app-2-$i" >> appset-2/values.yaml
+    cp -r list-app-3 list-app-4-$i
+    find list-app-4-$i/ -type f | xargs sed -i "s/3/$i/g"
+    echo "  list-app-4-$i:" >> appset-4/values.yaml
+    echo "    repoURL: https://github.com/michaelkotelnikov/argocd-bench-list-generator.git" >> appset-4/values.yaml
+    echo "    targetRevision: master" >> appset-4/values.yaml
+    echo "    path: list-app-4-$i" >> appset-4/values.yaml
 done
